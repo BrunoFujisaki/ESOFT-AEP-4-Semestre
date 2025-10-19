dc4e0d803e0282355865c423f77700e5718065da
BrunoFujisaki <brunofujisaki2004@gmail.com>
2025-10-19 19:28:32 -0300

    Criação da entidade Aluno

---
+++ src/main/java/model/Aluno.java
@@ -0,0 +1,46 @@
+package model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "usuarios")
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private LocalDate dataNascimento;
+
+    public Aluno() {
+    }
+
+    public Aluno(String nome, String email, String senha, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+}
