e95d466580ea92b637a43663fb362b02c11f727d
BrunoFujisaki <brunofujisaki2004@gmail.com>
2025-10-19 19:28:17 -0300

    Create table Alunos_Cursos

---
+++ src/main/resources/db/migration/V4__Create_Table_Alunos_Cursos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Alunos_Cursos(
+    aluno_id BIGINT NOT NULL,
+    curso_id BIGINT NOT NULL,
+    FOREIGN KEY (aluno_id) REFERENCES Alunos(id),
+    FOREIGN KEY (curso_id) REFERENCES Cursos(id),
+    PRIMARY KEY (aluno_id, curso_id)
+);
