b29ba84a21da6da7ad99af78ae701247210fab75
BrunoFujisaki <brunofujisaki2004@gmail.com>
2025-10-19 22:00:45 -0300

    Instanciando classe scanner e controller de alunos;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
+import controller.AlunoController;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AlunoController ac = new AlunoController(sc);
+        ac.iniciarMenu();
+        sc.close();
 
     }
 }
