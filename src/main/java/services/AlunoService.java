a293259d361eef2674f1299587f9dbc00725cafd
BrunoFujisaki <brunofujisaki2004@gmail.com>
2025-11-07 21:38:03 -0300

    Método para buscar aluno pela suas credencias na classe service

--- src/main/java/services/AlunoService.java
+++ src/main/java/services/AlunoService.java
@@ -1,33 +1,58 @@
 package services;
 
 import dao.AlunoDao;
+import dto.AlunoDTO;
 import model.Aluno;
 import util.JPAUtil;
 
 import javax.persistence.EntityManager;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class AlunoService {
 
     public AlunoService() {}
 
-    public void cadastrarAluno(Aluno aluno) {
+    public void cadastrarAluno(AlunoDTO alunoDTO) {
         EntityManager em = JPAUtil.getEntityManager();
         AlunoDao alunoDao = new AlunoDao(em);
-
         try {
+            var dataConvertida = LocalDate.parse(alunoDTO.dataNascimento(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
             em.getTransaction().begin();
-            alunoDao.save(aluno);
+            alunoDao.save(new Aluno(
+                    alunoDTO.nome(),
+                    alunoDTO.email(),
+                    alunoDTO.senha(),
+                    dataConvertida
+                    )
+            );
             em.getTransaction().commit();
+            System.out.println("✅ Aluno cadastrado com sucesso!");
         } catch (Exception ex) {
             if (em.getTransaction().isActive()) {
                 em.getTransaction().rollback();
             }
-            throw new RuntimeException("Erro ao cadastrar aluno.", ex);
+            System.err.println("❌ Erro ao cadastrar: " + ex.getMessage());
         } finally {
             if (em.isOpen()) {
                 em.close();
             }
+        }
+    }
+
+    public Aluno getAlunoPorCredenciais(String email, String senha) {
+        EntityManager em = JPAUtil.getEntityManager();
+        AlunoDao alunoDao = new AlunoDao(em);
+
+        try {
+            return alunoDao.getAlunoByCredenciais(email, senha);
+        }catch (Exception e) {
+            System.err.println("❌ Credenciais inválidas. Tente novamente.");
+            return null;
+        }finally {
+            if (em.isOpen())
+                em.close();
         }
     }
 
@@ -40,8 +65,10 @@ public class AlunoService {
             alunos = dao.findAllAlunos();
             return alunos;
         } catch (Exception e) {
-
             throw new RuntimeException("Falha ao buscar alunos.", e);
+        }finally {
+            if (em.isOpen())
+                em.close();
         }
     }
 }
