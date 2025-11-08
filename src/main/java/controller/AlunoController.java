b351878797454d49d5108289dc41ff6e338c717e
BrunoFujisaki <brunofujisaki2004@gmail.com>
2025-11-07 21:34:29 -0300

    Melhorando menu de alunos

--- src/main/java/controller/AlunoController.java
+++ src/main/java/controller/AlunoController.java
@@ -1,42 +1,63 @@
 package controller;
 
-import services.AlunoService;
-
-import java.util.Scanner;
+import dto.AlunoCursoDTO;
+import model.Aluno;
+import services.AlunoCursoService;
+import util.ScannerUtil;
 
 public class AlunoController {
-    private final AlunoService alunoService = new AlunoService();
-    private final Scanner sc;
+    private static final AlunoCursoService alunoCursoService = new AlunoCursoService();
 
-    public AlunoController(Scanner sc) {
-        this.sc = sc;
-    }
+    private AlunoController() {}
 
-    public void iniciarMenu() {
-        Scanner sc = new Scanner(System.in);
-        int opcao = 0;
+    public static void exibirTelaInicialAluno(Aluno aluno) {
+        int opcao;
+
         do {
-            System.out.println("\n--- MENU ALUNOS ---");
-            System.out.println("1. Listar Alunos");
-            System.out.println("0. Voltar");
-            System.out.print("Escolha: ");
-            opcao = sc.nextInt();
-            sc.nextLine();
+            System.out.println("\n=== MENU DO ALUNO ===");
+            System.out.printf("Bem-vindo, %s!\n", aluno.getNome());
+            System.out.println("=========================");
+            System.out.println("1. Ver cursos disponíveis");
+            System.out.println("2. Matricular-se em um curso");
+            System.out.println("3. Ver meus cursos");
+            System.out.println("4. Sair de um curso");
+            System.out.println("0. Sair");
+            System.out.print("Escolha uma opção: ");
 
+            opcao = ScannerUtil.lerInt();
             switch (opcao) {
-                case 1:
-                    listar();
-                    break;
-                case 2:
-
-                    break;
-                // ...
+                case 1 -> CursoController.listarCursos();
+                case 2 -> matricular(aluno);
+                case 3 -> verMeusCursos(aluno);
+                case 4 -> sairDeCurso(aluno);
+                case 0 -> System.out.println("\nSaindo do menu do aluno...");
+                default -> System.err.println("❌ Opção inválida. Tente novamente.");
             }
         } while (opcao != 0);
     }
 
-    private void listar() {
-        var alunos = alunoService.getAlunos();
-        alunos.forEach(a -> System.out.println(a.getNome()));
+    private static void matricular(Aluno aluno) {
+        System.out.println("\n--- MATRÍCULA EM CURSO ---");
+        System.out.print("Digite o ID do curso em que deseja se matricular: ");
+        long cursoId = ScannerUtil.lerInt();
+        alunoCursoService.matricularAlunoEmCurso(new AlunoCursoDTO(aluno.getId(), cursoId));
     }
+
+    private static void verMeusCursos(Aluno aluno) {
+        System.out.println("\n--- SEUS CURSOS ---");
+        var cursos = alunoCursoService.getCursosPorAlunoId(aluno.getId());
+        cursos.forEach(c -> {
+            System.out.println("- Id: "+c.getId()+", Nome: "+c.getNome()+", Carga Horária: "+c.getCargaHoraria());
+        });
+        ScannerUtil.lerLinha();
+    }
+
+    private static void sairDeCurso(Aluno aluno) {
+        System.out.println("\n--- SAIR DE UM CURSO ---");
+        System.out.println("Digite o ID do curso que voçe deseja sair:");
+        long cursoId = ScannerUtil.lerInt();
+        alunoCursoService.cancelarMatricula(aluno.getId(), cursoId);
+    }
+
+
 }
